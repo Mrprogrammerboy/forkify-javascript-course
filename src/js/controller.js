dbf35c8783ee725340b86e69e78cedf8b1ef585b
Mrprogrammerboy <andrewkirkbright27@gmail.com>
2026-03-26 11:50:25 +0000

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 ///////////////////////////////////////
 
 const controlRecipes = async function () {
@@ -65,7 +61,6 @@ const controlSearchResults = async function () {
 
 const controlPagination = function (goToPage) {
   // 1) Render NEW results
-  // resultsView.render(model.state.search.results);
   resultsView.render(model.getSearchResultsPage(goToPage));
 
   // 2) Render NEW pagination buttons
@@ -77,7 +72,6 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
@@ -128,10 +122,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -140,6 +130,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
